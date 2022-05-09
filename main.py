d08516b43004e096d35b8ab35673a19c0d786bdb
n3dhir <84473151+n3dhir@users.noreply.github.com>
2022-05-09 20:36:10 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -29,20 +29,20 @@ class button():
 
 BFS_button = button((206, 121, 159), 400, 500, 100, 35, "BFS")
 bfs_visite = button((206, 121, 159), 400, 550, 100, 35, "0")
-bfs_chemin = button((206, 121, 159), 400, 600, 100, 35, "+OO")
+bfs_chemin = button((206, 121, 159), 400, 600, 100, 35, "-----")
 
 DFS_button = button((206, 121, 159), 550, 500, 100, 35, "DFS")
 dfs_visite = button((206, 121, 159), 550, 550, 100, 35, "0")
-dfs_chemin = button((206, 121, 159), 550, 600, 100, 35, "+OO")
+dfs_chemin = button((206, 121, 159), 550, 600, 100, 35, "-----")
 
 
 A_STAR_button = button((206, 121, 159), 700, 500, 100, 35, "A*")
 a_star_visite = button((206, 121, 159), 700, 550, 100, 35, "0")
-a_star_chemin = button((206, 121, 159), 700, 600, 100, 35, "+OO")
+a_star_chemin = button((206, 121, 159), 700, 600, 100, 35, "-----")
 
-MANHATTAN_button = button((206, 121, 159), 850, 500, 100, 35, "MANHATTAN")
-manhattan_visite = button((206, 121, 159), 850, 550, 100, 35, "0")
-manhattan_chemin = button((206, 121, 159), 850, 600, 100, 35, "+OO")
+PROF_LIM_button = button((206, 121, 159), 850, 500, 100, 35, "PROF LIM L = 60")
+prof_lim_visite = button((206, 121, 159), 850, 550, 100, 35, "0")
+prof_lim_chemin = button((206, 121, 159), 850, 600, 100, 35, "-----")
 
 
 
@@ -59,63 +59,60 @@ longueur_chemin = button((206, 121, 159), 280, 600, 110, 35, "longueur du chemin
 
 images = []
 
-# initial = [1, 2, 3, 4, 5, 6, 0, 7, 8]
-# final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+initial = [1, 2, 3, 4, 5, 6, 0, 7, 8]
+final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
 # initial = [0, 2, 3, 1, 8, 5, 4, 7, 6]
 # final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
 
-initial = [1, 2, 7, 8, 5, 0, 3, 6, 4]
-final = [1, 2, 3, 8, 0, 4, 7, 6, 5]
+# initial = [1, 2, 7, 8, 5, 0, 3, 6, 4]
+# final = [1, 2, 3, 8, 0, 4, 7, 6, 5]
 
 
-# initial = [1, 4, 7, 8, 5, 2, 0, 3, 6]
-# final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-
-# initial = [0, 1, 2, 5, 6, 3, 4, 7, 8]
-# final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+initial = [0, 1, 2, 5, 6, 3, 4, 7, 8] #good one but not for dfs
+final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
 
 
-# initial = [0, 2, 1, 3, 7, 5, 8, 6, 4]
+# initial = [0, 2, 1, 3, 7, 5, 8, 6, 4] #good for bfs and a*
 # final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
 
-# initial = [5, 3, 4, 2, 1, 6, 8, 7, 0]
-# final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-
-
-# initial = [5, 2, 1, 3, 7, 0, 8, 6, 4]
-# final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+initial = [5, 3, 4, 2, 1, 6, 8, 7, 0]#good for bfs and a* and prof lim
+final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
 
 # initial = [2, 3, 1, 5, 4, 8, 7, 6, 0]
 # final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
 
-# initial = [5, 2, 1, 3, 7, 0, 8, 6, 4]
-# final = [0, 2, 1, 3, 7, 5, 8, 6, 4]
+initial = [5, 2, 1, 3, 7, 0, 8, 6, 4]#there is a trick try 63 instead of 60
+final = [0, 2, 1, 3, 7, 5, 8, 6, 4]
 
 
-initial = [1, 2, 7, 8, 5, 0, 3, 6, 4]
-final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
+# initial = [1, 3, 2, 4, 7, 5, 8, 0, 6]
+# final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
-initial = [1, 3, 2, 4, 7, 5, 8, 0, 6]
-final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-
-initial = [4, 3, 1, 7, 2, 6, 8, 5, 0]
-final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+# initial = [4, 3, 1, 7, 2, 6, 8, 5, 0]
+# final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
 
 # initial = [3, 0, 6, 5, 4, 1, 8, 7, 2]
 # final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
 
-initial = [5, 2, 7, 1, 0, 8, 4, 6, 3]
-final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+# initial = [5, 1, 3, 7, 8, 6, 4, 0, 2]
+# final = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
+
+# initial = [1, 7, 6, 3, 2, 4, 8, 5, 0]#good for dfs
+# final = [2, 1, 0, 4, 6, 7, 3, 8, 5]
+
+
+# initial = [1, 2, 7, 8, 5, 0, 3, 6, 4]
+# final = [1, 2, 3, 8, 0, 4, 7, 6, 5]
 taquin = copy.deepcopy(initial)
 
 
@@ -124,8 +121,9 @@ def shuffle():
   for i in range(100):
     x = random.randint(0, 8)
     y = random.randint(0, 8)
-    taquin[x], taquin[y] = taquin[y], taquin[x]
+    initial[x], initial[y] = initial[y], initial[x]
     images[x], images[y] = images[y], images[x]
+  taquin = reset()
 
 def reset(state = initial):
   taquin = copy.deepcopy(state)
@@ -136,29 +134,32 @@ def reset(state = initial):
 
 def moves(pos):
   t = []
-  if(pos != 2 and pos != 5 and pos != 8):
-    t.append(1)
-  if(pos != 0 and pos != 3 and pos != 6):
-    t.append(-1)
-  if(pos != 0 and pos != 1 and pos != 2):
-    t.append(-3)
+  
+  
+  
   if(pos != 6 and pos != 7 and pos != 8):
     t.append(3)
+  if(pos != 0 and pos != 1 and pos != 2):
+    t.append(-3)
+  if(pos != 0 and pos != 3 and pos != 6):
+    t.append(-1)
+  if(pos != 2 and pos != 5 and pos != 8):
+    t.append(1)
+
   return t
 
-def track(ancestor, node, val, zone):
+def track(ancestor, node, latency, zone):
   pile = []
   pile.append(node)
   while ancestor[str(node)] != node:
     node = ancestor[str(node)]
     pile.append(node)
-  zone.text = str(len(pile))
+  zone.text = str(len(pile) - 1)
   while pile:
     node = pile[-1]
     pile.pop(-1)
     taquin = reset(node)
-    draw(val)
-  # print("taquin: ", taquin)
+    draw(latency)
 
 def swap(tab, val):
   x = tab.index(0)
@@ -167,6 +168,7 @@ def swap(tab, val):
   return t
 
 def bfs():
+  global taquin
   n = 0
   visited = set()
   ancestor = dict()
@@ -178,12 +180,11 @@ def bfs():
     n += 1
     aux = queue[0]
     queue.pop(0)
-    # print(aux)
     if(aux == final):
-      # print(n)
       bfs_visite.text = n
       track(ancestor, aux, 250, bfs_chemin)
-      break
+      taquin = final
+      return
     x = aux.index(0)
     for i in moves(x):
       state = swap(aux, i)
@@ -191,40 +192,44 @@ def bfs():
         queue.append(state)
         visited.add(str(state))
         ancestor[str(state)] = aux
+  bfs_visite.text = n
+  bfs_chemin.text = "+OO"
 
 
 
 
 
 def dfs():
+  global taquin
   visited = set()
   ancestor = dict()
   ancestor[str(taquin)] = taquin
   pile = []
   pile.append(taquin)
   n = 0
+  visited.add(str(taquin))
   while pile:
     n += 1
     state = pile[-1]
     pile.pop(-1)
-    # print(state)
-    visited.add(str(state))
     if(state == final):
-      # print(n)
       dfs_visite.text = n
       track(ancestor, state, 0, dfs_chemin)
+      taquin = final
       return
     x = state.index(0)
     for i in moves(x):
-        aux = swap(state, i)
-        if str(aux) not in visited:
-          ancestor[str(aux)] = state
-          pile.append(aux)
+      aux = swap(state, i)
+      if str(aux) not in visited:
+        visited.add(str(aux))
+        ancestor[str(aux)] = state
+        pile.append(aux)
+  dfs_visite.text = n
+  dfs_chemin.text = "+OO"
 
 
 
 def malplace(state):
-  # print("haha",state, final)
   nb = 0
   for i in range(9):
     if state[i] != final[i]:
@@ -234,28 +239,13 @@ def malplace(state):
 
 
 
-def distance(state):
-  nb = 0
-  for i in range(9):
-    y = i % 3
-    x = (i - y) / 3
-    pos = final.index(state[i])
-    xx = pos % 3
-    yy = (pos - xx) / 3
-    nb += abs(x - xx) + abs(y - yy)
-  return nb
 
 
 
 
 
-
-
-
-
-
-
-def heuristique(typedeheuristique):
+def heuristique():
+  global taquin
   n = 0
   openset = set()
   closedset = set()
@@ -265,7 +255,7 @@ def heuristique(typedeheuristique):
   Current = taquin
   strCurrent = str(Current)
   G[strCurrent] = 0
-  H[strCurrent] = typedeheuristique(Current)
+  H[strCurrent] = malplace(Current)
 
   openset.add(strCurrent)
   parent[strCurrent] = Current
@@ -294,21 +284,15 @@ def heuristique(typedeheuristique):
       if i == '0':
         Current.append(0)
     if(Current == final):
-      # print(n)
-      if typedeheuristique == distance:
-        manhattan_visite.text = n
-        track(parent, Current, 250, manhattan_chemin)
-      else:
-        a_star_visite.text = n
-        track(parent, Current, 250, a_star_chemin)
-      break
+      a_star_visite.text = n
+      track(parent, Current, 250, a_star_chemin)
+      taquin = final
+      return
     strCurrent = str(Current)
     openset.remove(strCurrent)
     closedset.add(strCurrent)
 
     for i in moves(Current.index(0)):
-      # print("current: ", Current)
-      # print(i)
       node = swap(Current, i)
       strNode = str(node)
       if strNode in closedset:
@@ -320,9 +304,45 @@ def heuristique(typedeheuristique):
           parent[strNode] = Current
       else:
         G[strNode] = G[strCurrent] + 1
-        H[strNode] = typedeheuristique(node)
+        H[strNode] = malplace(node)
         parent[strNode] = Current
         openset.add(strNode)
+  a_star_visite.text = n
+  a_star_chemin.text = "+OO"
+
+
+
+
+def prof_lim(val):
+  global taquin
+  visited = set()
+  ancestor = dict()
+  ancestor[str(taquin)] = taquin
+  pile = []
+  pile.append((taquin, 0))
+  visited.add(str(taquin))
+  n = 0
+  while pile:
+    n += 1
+    state = pile[-1]
+    pile.pop(-1)
+    if(state[0] == final):
+      prof_lim_visite.text = n
+      track(ancestor, state[0], 250, prof_lim_chemin)
+      taquin = final
+      return
+    if(state[1] == val):
+      continue
+    x = state[0].index(0)
+    for i in moves(x):
+      aux = swap(state[0], i)
+      if str(aux) not in visited:
+        ancestor[str(aux)] = state[0]
+        pile.append((aux, state[1] + 1))
+        visited.add(str(aux))
+  prof_lim_visite.text = n
+  prof_lim_chemin.text = "+OO"
+
 
 
 
@@ -373,9 +393,9 @@ while running:
   a_star_visite.draw(window)
   a_star_chemin.draw(window)
 
-  MANHATTAN_button.draw(window)
-  manhattan_visite.draw(window)
-  manhattan_chemin.draw(window)
+  PROF_LIM_button.draw(window)
+  prof_lim_visite.draw(window)
+  prof_lim_chemin.draw(window)
 
   SHUFFLE_button.draw(window)
   RESET_button.draw(window)
@@ -396,26 +416,26 @@ while running:
       if DFS_button.isOver(pos):
         dfs()
       if A_STAR_button.isOver(pos):
-        heuristique(malplace)
-      if MANHATTAN_button.isOver(pos):
-        heuristique(distance)
+        heuristique()
+      if PROF_LIM_button.isOver(pos):
+        prof_lim(60)
       if SHUFFLE_button.isOver(pos):
         bfs_visite.text = "0"
-        bfs_chemin.text = "+OO"
+        bfs_chemin.text = "-----"
 
         dfs_visite.text = "0"
-        dfs_chemin.text = "+OO"
+        dfs_chemin.text = "-----"
 
 
         a_star_visite.text = "0"
-        a_star_chemin.text = "+OO"
+        a_star_chemin.text = "-----"
 
-        manhattan_visite.text = "0"
-        manhattan_chemin.text = "+OO"
+        prof_lim_visite.text = "0"
+        prof_lim_chemin.text = "-----"
         shuffle()
 
       if RESET_button.isOver(pos):
-        taquin = reset(taquin)
+        taquin = reset()
     if event.type == pygame.QUIT:
       pygame.quit()
       running = False
